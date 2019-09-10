0abdd9b944778add9c03e68e4dee4f12d71cd010
shu <user@aoki-no-MacBook-Air.local>
2019-09-10 20:10:00 +0900

    backup script 20190910

--- backup.sh
+++ backup.sh
@@ -1,27 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 BK_DATE=`date +'%Y%m%d'`
 BK_NAME=/home/vagrant/backup
+DL_DATE=`/bin/date -d '7 day ago' +%Y%m%d`
 
-mysqldump --single-transaction -u root -pAokipiza0027# menta > $BK_NAME/menta_$BK_DATE.dump
+mysqldump --single-transaction -u root -pAokipiza0027# menta > $BK_NAME/menta_$BK_DATE.dump > /dev/null 2>&1
 
-aws s3 cp $BK_NAME/menta_$BK_DATE.dump s3://menta-20190909/
+aws s3 cp $BK_NAME/menta_$BK_DATE.dump s3://menta-bkup/
 
-aws s3 ls s3://menta-20190909 > list.log
-num=`cat list.log |awk 'END {print NR}'`
-num=`expr $num - 7`
-echo "delete num = $num"
-echo "delete list"
-cat list.log |head -n $num
-
-for target in `seq 1 $num`
-	do
-  rmfile=`cat list.log |awk '{print $4}' | head -n $target`
-	done
-	
-echo $rmfile
-  
-for delete in $rmfile
-	do
-  aws s3 rm s3://menta-20190909/$delete
-	done
+aws s3 rm s3://menta-bkup/menta_$DL_DATE.dump
